f27d4154c3063ecaaa2fac35d2cf16a76329a49e
Jamshaa <125591505+Jamshaa@users.noreply.github.com>
2025-12-11 19:50:58 +0100

    Update security filter chain for authorization rules
    S'han afegit noves regles per inventory i s'han posat les regles que ja estaven definides a main

--- src/main/java/cat/udl/eps/softarch/demo/config/WebSecurityConfig.java
+++ src/main/java/cat/udl/eps/softarch/demo/config/WebSecurityConfig.java
@@ -27,10 +27,29 @@ public class WebSecurityConfig {
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers(HttpMethod.GET, "/businesses", "/businesses/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/businesses").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/businesses/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PATCH, "/businesses/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/businesses/**").hasRole("ADMIN")
+                
+                .requestMatchers(HttpMethod.GET, "/inventories/**").authenticated()
+                .requestMatchers(HttpMethod.POST, "/inventories").hasRole("BUSINESS")
+                .requestMatchers(HttpMethod.PUT, "/inventories/**").hasRole("BUSINESS")
+                .requestMatchers(HttpMethod.PATCH, "/inventories/**").hasRole("BUSINESS")
+                .requestMatchers(HttpMethod.DELETE, "/inventories/**").hasRole("BUSINESS")
+
                 .requestMatchers(HttpMethod.GET, "/identity").authenticated()
                 .requestMatchers(HttpMethod.POST, "/users").anonymous()
                 .requestMatchers(HttpMethod.POST, "/users/*").denyAll()
+                .requestMatchers(HttpMethod.POST, "/customers").anonymous()
+                .requestMatchers(HttpMethod.POST, "/customers/*").denyAll()
+                .requestMatchers(HttpMethod.GET, "/orders").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/orders/*").authenticated()
+                .requestMatchers(HttpMethod.POST, "/orders").hasRole("CUSTOMER")
+                
                 .requestMatchers(HttpMethod.POST, "/*").authenticated()
+                .requestMatchers(HttpMethod.POST, "/*/*").authenticated()
                 .requestMatchers(HttpMethod.PUT, "/*/*").authenticated()
                 .requestMatchers(HttpMethod.PATCH, "/*/*").authenticated()
                 .requestMatchers(HttpMethod.DELETE, "/*/*").authenticated()
